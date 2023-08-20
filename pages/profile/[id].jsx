d355d3ee72d5e42a512f862e8eeeaa871a517f07
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2023-08-21 00:49:03 +0300

    add sign out function to profile page

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 const index = () => {
+    const { data: session } = useSession()
+    const { push } = useRouter()
+
+
+    // create handle signout function
+    const handleSignOut = () => {
+        if (confirm("are you sure")) {
+            signOut({ redirect: false })
+        }
+    }
+
+    useEffect(() => {
+        if (!session) {
+            push("/auth/login")
+        }
+    }, [session, push])
+
+
+
+
+
     return (
         <div className="text-center text-3xl capitalize">
-            user profile
-            <button>exit</button>
+            <div>user profile</div>
+            <div className='container mx-auto flex justify-start'>
+                <div
+                    onClick={handleSignOut}
+                    className="cursor-pointer">exit</div>
+            </div>
         </div>
     )
 }
